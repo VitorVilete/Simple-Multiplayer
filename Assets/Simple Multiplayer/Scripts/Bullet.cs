9250c30df2190ed79461b20f0be59ab82c79a3eb
Vitor Vilete <vitorgarcesvilete@gmail.com>
2016-05-11 17:56:39 -0300

    First Build

---
+++ Assets/Simple Multiplayer/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col) {
+        GameObject hit = col.gameObject;
+        Health health = hit.GetComponent<Health>();
+        //if bullet hits something that has health
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+        Destroy(gameObject);
+    }
+}
